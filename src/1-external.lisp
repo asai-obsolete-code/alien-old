e3e353e2646aadd6809329a176f63dc8cc01abc1
Masataro Asai <guicho2.71828@gmail.com>
2017-04-10 09:40:51 +0900

    translation only

--- src/1-external.lisp
+++ src/1-external.lisp
@@ -6,7 +6,8 @@
 (defun fd-preprocess (domain problem)
   ($ "echo $PWD" :output t :error-output t)
   ($ #?"time ${*fd-home*}/fast-downward.py --translate ${domain} ${problem}" :output t :error-output t)
-  ($ #?"time ${*fd-home*}/fast-downward.py --preprocess output.sas" :output t :error-output t)
-  ;; ($ #?"mv output ${path}")
-  (sas-parser:parse "output"))
+  (sas-parser:parse "output.sas")
+  #+nil ($ #?"time ${*fd-home*}/fast-downward.py --preprocess output.sas" :output t :error-output t)
+  #+nil ($ #?"mv output ${path}")
+  #+nil (sas-parser:parse "output"))
 
