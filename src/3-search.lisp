38ec2c5e550bce03905f5c2b0d323c22fa1e895a
Masataro Asai <guicho2.71828@gmail.com>
2017-04-15 11:32:19 +0900

    bfs

--- src/3-search.lisp
+++ src/3-search.lisp
@@ -3,5 +3,15 @@
 
 (defvar *task*)
 (defun run-search (*task*)
-  )
+  "BFS"
+  (match *task*
+    ((task init-op goal-op operators axioms)
+     (let ((*states* (zdd-emptyset)))
+       (setf *states* (apply-op init-op *states*))
+       (iter (unless (node-equal (zdd-emptyset) (apply-axiom goal-op *states*))
+               (signal 'solution-found))
+             (iter (for op in operators)
+                   (setf *states* (apply-op op *states*)))
+             (iter (for ax in axioms)
+                   (setf *states* (apply-axiom ax *states*))))))))
 
