4f81641c7b9a1daab02d93a1875eafcacee128fd
Masataro Asai <guicho2.71828@gmail.com>
2017-04-18 18:35:13 +0900

    break+

--- src/0-package.lisp
+++ src/0-package.lisp
@@ -50,3 +50,16 @@
 
 (defun dump (zdd)
   (map-ones zdd #'print))
+
+(defmacro break+ (&rest args)
+  (let* ((last-form (lastcar args))
+         (last last-form)
+         (butlast (butlast args)))
+    (once-only (last)
+      `(progn
+         (break "~@{~a~2%~<;;~@; result:~4i~:@_~a~;~:>~2%~}"
+                ,@(iter (for arg in butlast)
+                        (collect `',arg)
+                        (collect `(list ,arg)))
+                ',last-form (list ,last))
+         ,last))))
