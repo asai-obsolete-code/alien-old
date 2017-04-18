5ce9dbd6ee5d5175a781d469a646f259eae24d46
Masataro Asai <guicho2.71828@gmail.com>
2017-04-18 14:42:42 +0900

    decode schema

--- src/3-decode.lisp
+++ src/3-decode.lisp
@@ -1,3 +1,13 @@
 
 (in-package :alien)
 
+
+
+(defun decode-parent-op (states schema)
+  (let ((from (schema-index schema :action))
+        (to   (schema-index schema :cost))
+        acc)
+    (map-ones states
+              (lambda (bv)
+                (push (subseq bv from to) acc)))
+    acc))
