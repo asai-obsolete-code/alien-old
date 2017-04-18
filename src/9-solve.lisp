b5eda0689a0c72287feeb0de3673404fc4fd9be5
Masataro Asai <guicho2.71828@gmail.com>
2017-04-18 16:38:50 +0900

    playground

--- src/9-solve.lisp
+++ src/9-solve.lisp
@@ -8,4 +8,12 @@
       (encode-sas-to-zdd)
       (run-search))))
 
-;; (solve (asdf:system-relative-pathname :alien "sets/misc-strips/movie/p001.pddl"))
+
+;; (defparameter *sas* (fd-preprocess #p"sets/aaai16-opt/blocks/p01.pddl"))
+(setf *sas* (fd-preprocess (asdf:system-relative-pathname :alien "sets/misc-adl/psr-middle/p01.pddl")))
+(defparameter *movie* (asdf:system-relative-pathname :alien "sets/misc-strips/movie/p001.pddl"))
+(defparameter *movie-sas* (fd-preprocess *movie*))
+(defparameter *movie-task* (encode-sas-to-zdd *movie-sas*))
+(nop
+  
+  (solve ))
